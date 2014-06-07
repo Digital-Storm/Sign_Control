4295aa71df3a35e2d1a3e864108515155d02f4b3
Daniel <digitalstorm07@gmail.com>
2014-06-07 01:43:40 -0400

    odds and ends changes

--- WindowsApplication1/Signs.vb
+++ WindowsApplication1/Signs.vb
@@ -1,3 +1,45 @@
-﻿Public Class Signs
+﻿Class Signs
+    Private sName As String
+    Private sBrightness As String
+    Private sDiffs As String
+    Private sKey As String
+    Private sHeader As String
+    Private m_Settings As Hashtable
 
+    Public Overrides Function ToString() As String
+        Return sName
+    End Function
+
+    Public Function Brightness() As String
+        Return sBrightness
+    End Function
+
+    Public Function Diffs() As String
+        Return sDiffs
+    End Function
+
+    Public Function Key() As String
+        Return sKey
+    End Function
+
+    Public Function Header() As String
+        Return sHeader
+    End Function
+
+    'Class Type
+    '    Inherits IP
+    '    Public Sub Galaxy()
+
+    '    End Sub
+    '    Public Sub DF_2053()
+
+    '    End Sub
+    'End Class
+
+    Class Settings
+
+
+
+    End Class
 End Class
+
